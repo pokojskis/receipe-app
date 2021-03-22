a02943eb76508be0425fb44dade68f82fc1dde74
Szymon Pokojski <szymon.pokojski@lppsa.com>
2021-03-22 21:31:35 +0100

    feat: init commit

---
+++ src/main/java/com/sektorsoftware/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sektorsoftware.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
